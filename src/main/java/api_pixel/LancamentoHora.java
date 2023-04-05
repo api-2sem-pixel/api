b0d5bbae99c9fd349c00cfaac59f589e3d4488b3
wdeus <wagner.silva96@fatec.sp.gov.br>
2023-04-04 22:10:37 -0300

    Correcao javafx

---
+++ src/main/java/api_pixel/LancamentoHora.java
@@ -0,0 +1,20 @@
+package api_pixel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class LancamentoHora {
+
+	@FXML
+	private Button btn_lancar;
+	
+	@FXML
+	private TableView<?> table_lancamento;
+	
+	@FXML
+	void lancarHoras(ActionEvent event) {
+		
+	}
+}
