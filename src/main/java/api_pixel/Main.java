238bf72aa8b6d7a281047e767d474356263f0d5c
alisson <alisson.pereira5@fatec.sp.gov.br>
2023-04-15 22:17:12 -0300

    melhorias tela de cadastro de usuario

--- src/main/java/api_pixel/Main.java
+++ src/main/java/api_pixel/Main.java
@@ -21,12 +21,10 @@ public class Main extends Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
+        stage.setTitle("Cadastro de novo usuario");
         Scene scene = new Scene(root, 944, 609);
         stage.setScene(scene);
         
-        var stylesPath = getClass().getResource("/view/stylesCadastroUsuario.css").toString();
-        stage.getScene().getStylesheets().add(stylesPath);
         
         stage.show();
     }
