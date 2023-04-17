e2d9489921757cd0a3284dbea65574fc3ba9e6c8
Alisson dos Santos Pereira <111581261+4l1son@users.noreply.github.com>
2023-04-17 20:17:04 -0300

    Add files via upload

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,9 +1,16 @@
 package controller;
 
+import java.io.IOException;
+
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class LoginController {
@@ -17,16 +24,33 @@ public class LoginController {
 	
 	
     @FXML
-    private Button btn_entrar;
+    private static Button btn_entrar;
 
 	@FXML
-    private void entrarUsuario()   {
+    private void entrarUsuario(Stage stage)   {
 		
 		String email = emailField.getText();
     	String password = passwordField.getText();
-    	if (email.equals("admin") && password.equals("123")) {
-    		System.out.println(email);
+		String logado =  btn_entrar.getId();
+		System.out.println(logado);
+    	if (email.equals("cliente") && password.equals("senha")) {
+    		try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("/view/Menu/Menu.fxml"));
+                if(logado == "btn_entrar"){
+					Parent home = loader.load();
+				    Scene scene = new Scene(home, 944, 609);
+                    stage.setScene(scene);
+			     	stage.show();
+			  
+				}
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     	}
+		else{
+			//percorrer o banco com usuarios cadastrados
+		}
     	
     	
 
