ec5af76db81dc680d552e1685d83ec8bdb8565c3
matheus-fiebig <macrubig@gmail.com>
2023-04-17 21:31:04 -0300

    Login controller

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import java.io.IOException;
+
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class LoginController {
@@ -18,24 +24,45 @@ public class LoginController {
 	
 	
     @FXML
-    private Button btn_entrar;
+    private  Button btn_entrar;
 
 	@FXML
-    private void entrarUsuario()   {
+    private void entrarUsuario(Stage stage)   {
 		
 		String email = emailField.getText();
     	String password = passwordField.getText();
-    	if (email.equals("admin") && password.equals("123")) {
-    		;
+		
+		if (email.equals("cliente") && password.equals("senha")) {
+			Parent root = null;
+        
+        var resource = getClass()
+            .getResource("/view/Menu/Menu.fxml");
+        
+        try {
+            root =  FXMLLoader.load(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        Scene scene = new Scene(root, 944, 609);
+        stage.setScene(scene);
+        
+        var stylesPath = getClass().getResource("/view/styles.css").toString();
+        stage.getScene().getStylesheets().add(stylesPath);
+        
+        MenuController.setStage(stage);
+        stage.show();
     	}
+		else{
+			//percorrer o banco com usuarios cadastrados
+		}
     	
     	
 
 	}
-    }
+}
     	
 		
     	
   
-
 
