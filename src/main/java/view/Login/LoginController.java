9923cf6a67ad132e9a9a7e34696dd1a62108de31
Alisson dos Santos Pereira <111581261+4l1son@users.noreply.github.com>
2023-04-19 21:02:57 -0300

    Add files via upload
    Tela de login melhorias

---
+++ src/main/java/view/Login/LoginController.java
@@ -0,0 +1,78 @@
+package controller;
+
+import java.io.IOException;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+public class LoginController {
+
+    
+	@FXML
+	private TextField emailField;
+	
+	@FXML
+	private PasswordField passwordField;
+	
+	
+    @FXML
+    private  Button btn_entrar;
+
+	@FXML
+    private void entrarUsuario(Stage stage)   {
+		
+		String email = emailField.getText();
+    	String password = passwordField.getText();
+		
+		if (email.equals("cliente") && password.equals("senha")) {
+			Parent root = null;
+        
+        var resource = getClass()
+            .getResource("/view/Menu/Menu.fxml");
+        
+        try {
+            root =  FXMLLoader.load(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        Scene scene = new Scene(root, 944, 609);
+        stage.setScene(scene);
+        
+        var stylesPath = getClass().getResource("/view/styles.css").toString();
+        stage.getScene().getStylesheets().add(stylesPath);
+        
+        MenuController.setStage(stage);
+        stage.show();
+    	}
+		else{
+			//percorrer o banco com usuarios cadastrados
+		}
+        private MensagemRetorno msg = new MensagemRetorno();
+    	boolean status = false;
+        if((email.equals("cliente") && password.equals("senha")==status)){
+            msg.erro();
+            msg.limpar();   
+        }
+
+        public void limpar() {
+            this.tfNome.clear();
+            this.tfCodigo.clear();
+            this.tfSigla.clear();
+        }
+    	
+
+	}
+}
+    	
+		
+    	
+  
