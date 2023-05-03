f22141536ceb30944c52c2941f3ec6e862c2aba7
Alisson dos Santos Pereira <111581261+4l1son@users.noreply.github.com>
2023-05-02 22:25:28 -0300

    Update LoginController.java

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,8 +2,9 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import dao.TipoUsuarioDAO;
 import dao.UsuarioDAO;
+import enums.TipoUsuario;
 import factory.ConnectionFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import utils.mensagem_retorno.MensagemRetorno;
+import controller.MenuFeedBack.*;
 
 public class LoginController implements Initializable {   
 	@FXML
@@ -38,19 +40,28 @@ public class LoginController implements Initializable {
 		
         var usuario = usuarioDAO.getUsuarioBy(email);
 
+        var tipo_usuarioADM = TipoUsuario.values().equals(TipoUsuario.Administrador);
+        var tipo_usuarioGESTOR = TipoUsuario.values().equals(TipoUsuario.Gestor);
+        
         if(usuario == null){
             MensagemRetorno.erro("Email e/ou senha incorretos.");
             return;
         }
 
         if (email.trim().equals(usuario.getEmail().trim()) && password.equals(usuario.getCpf_cnpj().substring(0, 3))) {
+            if(tipo_usuarioADM == true || tipo_usuarioGESTOR == true){
             UsuarioDAO.usuarioLogado = usuario;
-			MenuController.irMenu();
+			MenuFeedBackController.irFeedBackHora();
             return;
+            }else{
+                UsuarioDAO.usuarioLogado = usuario;
+                MenuController.irMenu();
+                   
+            }
     	}
         
         MensagemRetorno.erro("Email e/ou senha incorretos.");
     }
 }
-    	  
+    	
 	
