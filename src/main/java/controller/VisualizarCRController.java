ac32d7cbe2110f28edefeae50e005742fee97248
wdeus <wagner.silva96@fatec.sp.gov.br>
2023-05-10 19:02:12 -0300

    botão delete

--- src/main/java/controller/VisualizarCRController.java
+++ src/main/java/controller/VisualizarCRController.java
@@ -7,16 +7,22 @@ import dao.CrDAO;
 import dao.CrUsuarioDAO;
 import dto.CrDTO;
 import dto.IntegrantesCrDTO;
+import enums.EtapaExtrato;
 import factory.ConnectionFactory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
+import model.ExtratoHoraModel;
+import utils.mensagem_retorno.MensagemRetorno;
 
 public class VisualizarCRController {
 
@@ -41,6 +47,9 @@ public class VisualizarCRController {
 	@FXML
 	private TableColumn<IntegrantesCrDTO, String> descricaoCol;
 	
+	@FXML
+	private TableColumn<IntegrantesCrDTO, Void> acoesCol;
+	
 	private ObservableList<CrDTO> cr = FXCollections.observableArrayList();
 	
 	@FXML
@@ -50,9 +59,13 @@ public class VisualizarCRController {
 		
 		integrantesCol.setCellValueFactory(new PropertyValueFactory<>("integrante"));
 		descricaoCol.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+		acoesCol.setCellValueFactory(new PropertyValueFactory<>(""));
+		
+		//adicionarBotaoDeletar();
 	}
 	
 	public void buscar(ActionEvent event) {
+		adicionarBotaoDeletar();
 		CrDTO cr = (CrDTO) comboCR.getSelectionModel().getSelectedItem();
 		List<IntegrantesCrDTO> integrantes = crUsuarioDAO.listarIntegrantes(cr.getId());
 		tabela.setItems(listaIntegrantes(integrantes));
@@ -65,5 +78,38 @@ public class VisualizarCRController {
     @FXML
     void retornarMenu(MouseEvent event) {
         MenuController.irMenu();
+    }
+    
+    public void adicionarBotaoDeletar() {
+        var buttonDeletar = new Callback<TableColumn<IntegrantesCrDTO, Void>, TableCell<IntegrantesCrDTO, Void>>() {
+            @Override
+            public TableCell<IntegrantesCrDTO, Void> call(final TableColumn<IntegrantesCrDTO, Void> param) {
+                final TableCell<IntegrantesCrDTO, Void> cell = new TableCell<IntegrantesCrDTO, Void>() {
+
+                    private final Button btn = new Button("Excluir");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            var row = getTableView().getItems().get(getIndex());
+                            getTableView().getItems().remove(getIndex());
+                            return;
+                        });
+                    }
+                    
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+        acoesCol.setCellFactory(buttonDeletar);
+
     }
 }
